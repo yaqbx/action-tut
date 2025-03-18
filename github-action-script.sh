bc761fba11cb2e9cb3a3edf408d3fd21b616a610
yaqbx <33007344+yaqbx@users.noreply.github.com>
2025-03-18 10:12:31 +0100

    Create github-action-script.sh
    bash script

---
+++ github-action-script.sh
@@ -0,0 +1,16 @@
+  echo 1. What is in the directory
+          ls -a
+          echo
+          echo 2. Is java installed
+          java -version
+          echo
+          echo 3. Disk space
+          df -h
+          echo
+          echo 4. Display environment for user
+          env
+          echo
+          echo 5. Whoami
+          whoami
+          echo
+          echo 6. $ANDROID_SDK_ROOT
